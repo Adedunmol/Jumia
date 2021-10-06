74cb93c8b6330d0b0ae9776c99fb6f3b0a35750c
Adedunmol <oyewaleadedunmola@gmail.com>
2021-10-06 19:56:34 +0100

    added flask-migrate and searchablemixin

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from website import create_app
+from website import create_app, db
 from search import add_to_index, remove_from_index, query_index
 from website.models import Post
+from flask_migrate import Migrate
 
 app = create_app()
+Migrate(app, db)
 
 @app.shell_context_processor
 def inject_functions():
-    return dict(add_to_index=add_to_index, remove_from_index=remove_from_index, query_index=query_index, Post=Post, app=app)
+    return dict(db=db, add_to_index=add_to_index, remove_from_index=remove_from_index, query_index=query_index, Post=Post, app=app)
 
 if __name__ == '__main__':
     app.run(debug=True)
