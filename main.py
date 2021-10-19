4004ed61f526fa7764e7209fc3757e3e5c5172d3
Adedunmol <oyewaleadedunmola@gmail.com>
2021-10-19 14:47:33 +0100

    adding command-line commands

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from website import create_app, db
 from search import add_to_index, remove_from_index, query_index
 from website.models import Post
 from flask_migrate import Migrate
+from website import cli
 
 app = create_app()
 Migrate(app, db)
